5d5a0280d60bc1b08ef25c172d75b423f0474f58
Rehanul hasan Bappy <rehanulh@gmail.com>
2025-07-26 00:10:06 +0200

    Installation done and Basic Setup Done

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Heading from "@/components/Heading";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         Hello Dev
         <button>Hello</button>
+        <Heading></Heading>
       </main>
     </div>
   );
---
+++ src/components/AboutContents.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutContents() {
+  return <div>About Content</div>;
+}
